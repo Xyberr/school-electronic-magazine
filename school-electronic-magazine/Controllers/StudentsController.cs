f5f00f98c23538f12b7a9ceba4939badc44762a0
Xyberr <makszacev238@gmail.com>
2025-09-27 00:19:38 +0500

    add role-based authorization to StudentsController

--- school-electronic-magazine/Controllers/StudentsController.cs
+++ school-electronic-magazine/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using school_electronic_magazine.DTO;
 using school_electronic_magazine.Models;
@@ -18,11 +19,13 @@ public class StudentsController : ControllerBase
     }
     
     [HttpGet]
+    [Authorize(Roles = "Admin,Teacher")]
     public async Task<IActionResult> GetAllStudents()
     {
         return Ok(await _studentService.GetAllStudentsAsync());
     }
     
+    [Authorize(Roles = "Admin,Teacher,Student")]
     [HttpGet("get_student_/{id}")]
     public async Task<IActionResult> GetStudentById(int id)
     {
@@ -37,18 +40,21 @@ public class StudentsController : ControllerBase
         }
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpPost("add_student")]
     public async Task<IActionResult> AddStudent(StudentDto studentDto)
     {
         return Ok(await _studentService.Create(studentDto));
     }
 
+    [Authorize(Roles = "Admin,Teacher")]
     [HttpPut("update_student")]
     public async Task<IActionResult> UpdateStudent(int id, [FromBody]StudentDto studentDto)
     {
         return Ok(await _studentService.Update(id, studentDto));
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpDelete("delete_student_/{id}")]
     public async Task<IActionResult> DeleteStudent(int id)
     {
