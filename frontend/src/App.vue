4d5739b6aad4c4a0bfc421c01452e41ded66a213
dhelfy <dhcode4business@gmail.com>
2025-09-27 15:44:24 +0500

    project initialization

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Button from 'primevue/button';
+
+const onClick = () => {
+  console.log("clicked")
+}
+</script>
+
+<template>
+  <Button label="Submit" @click="onClick" />
+</template>
+
+<style scoped></style>
