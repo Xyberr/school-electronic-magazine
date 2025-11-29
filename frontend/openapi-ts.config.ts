faf16ae6af5d6ed9d75ff865f97e31c9628f1759
dhelfy <dhcode4business@gmail.com>
2025-11-29 18:42:17 +0500

    chore: added env variable for swagger

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@hey-api/openapi-ts';
+import { loadEnv } from 'vite'
+
+const swaggerUrl = loadEnv('development', './').VITE_BACKEND_SWAGGER
+if (!swaggerUrl) {
+    throw new Error('API_SWAGGER_URL is not defined in environment variables');
+}
 
 export default defineConfig({
-  input: 'http://localhost:5000/swagger/v1/swagger.json',
+  input: swaggerUrl,
   output: 'src/heyapi',
 });
