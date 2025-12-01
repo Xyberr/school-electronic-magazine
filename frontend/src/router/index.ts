95f327be2580e491d07763ed55dc7c80cd2e92f9
dhelfy <dhcode4business@gmail.com>
2025-12-01 17:41:17 +0500

    feat(auth): added token check when user tries to visit /login;slight code refactoring

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,42 +1,59 @@
-import type { TokenPayload } from '@/types/auth'
-import { jwtDecode } from 'jwt-decode'
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+import { getJwtPayload } from "@/utils/jwt";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes, handleHotUpdate } from "vue-router/auto-routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
 if (import.meta.hot) {
-  handleHotUpdate(router)
+  handleHotUpdate(router);
 }
 
 // auth check
 router.beforeEach((to) => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
+
+  if (to.path == "/login") {
+    if (!token) {
+      return;
+    }
+
+    const decoded = getJwtPayload(token);
+    if (decoded === null) {
+      localStorage.removeItem("token");
+      return "/login";
+    }
+
+    const expired = decoded.exp * 1000 < Date.now();
+    if (expired) {
+      localStorage.removeItem("token");
+      return "/login?reason=expired";
+    }
+
+    return "/private";
+  }
 
   if (!to.meta.requiresAuth) {
-    return
+    return;
   }
 
   if (!token) {
-    return '/login'
+    return "/login";
   }
 
-  let decoded
-  try {
-    decoded = jwtDecode<TokenPayload>(token)
-  } catch (error) {
-    localStorage.removeItem('token')
-    return '/login'
+  const decoded = getJwtPayload(token);
+  if (decoded === null) {
+    localStorage.removeItem("token");
+    return "/login";
   }
 
-  const expired = decoded.exp * 1000 < Date.now()
+  const expired = decoded.exp * 1000 < Date.now();
   if (expired) {
-    localStorage.removeItem('token')
-    return '/login?reason=expired'
+    localStorage.removeItem("token");
+    return "/login?reason=expired";
   }
-})
+});
 
-export default router
+export default router;
