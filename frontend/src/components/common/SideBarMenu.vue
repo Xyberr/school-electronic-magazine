a464be58355e73348c5e603df571a5fda37a48de
dhelfy <dhcode4business@gmail.com>
2026-02-24 13:07:43 +0500

    feat: added component for sidebar

---
+++ frontend/src/components/common/SideBarMenu.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div class="sidebarMenu">
+        <span class="pi pi-bars"></span>
+    </div>
+</template>
+
+<style>
+.sidebarMenu {
+    height: 100vh;
+    background-color: red;
+    position: relative;
+    width: fit-content;
+    left: 0;
+}
+</style>
