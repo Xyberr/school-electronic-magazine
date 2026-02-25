0192b65760c09251008b59251dd0138d252cca4c
dhelfy <dhcode4business@gmail.com>
2026-02-25 16:22:56 +0500

    feat: pages now represented as an array of objects

--- frontend/src/components/common/SideBarLinks.vue
+++ frontend/src/components/common/SideBarLinks.vue
@@ -1,12 +1,22 @@
 <script setup>
+const pages = [
+    { name: 'Page 1', path: '/page1' },
+    { name: 'Page 2', path: '/page2' },
+    { name: 'Page 3', path: '/page3' },
+    { name: 'Page 4', path: '/page4' },
+]
 </script>
 
 <template>
    <div class="pagesList">
-        <RouterLink class="pagesListItem" to="/page1">Page 1</RouterLink>
-        <RouterLink class="pagesListItem" to="/page2">Page 2</RouterLink>
-        <RouterLink class="pagesListItem" to="/page3">Page 3</RouterLink>
-        <RouterLink class="pagesListItem" to="/page4">Page 4</RouterLink>
+        <RouterLink 
+            class="pagesListItem" 
+            v-for="page in pages" 
+            :key="page.name" 
+            :to="page.path"
+        >
+            {{ page.name }}
+        </RouterLink>
     </div>
 </template>
 
